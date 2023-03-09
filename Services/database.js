e79301f277cf076b8470d22bda4638157b538c8f
SadnanRashid <sadnan.rashid.07@gmail.com>
2023-03-09 22:20:02 +0600

    database connected

--- Services/database.js
+++ Services/database.js
@@ -3,7 +3,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // client.db('address-book').collection('contacts');
 
 const connectDatabase = () => {
-  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tvyjzb3.mongodb.net/?retryWrites=true&w=majority`;
+  const uri = `mongodb+srv://msmlead:Happy@123@cluster0.jq6jeg1.mongodb.net/?retryWrites=true&w=majority`;
   const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
