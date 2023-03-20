53712e695ad9c00055b5fb5330e8b827079a3dcf
SadnanRashid <sadnan.rashid.07@gmail.com>
2023-03-20 13:22:01 +0600

    adding reviews

---
+++ Controllers/Lawyer_Reviews/lawyer-reviews-controller.js
@@ -0,0 +1,30 @@
+const {
+  QueryAddUser,
+  QueryGetUser,
+  QueryUpdateUserDetails,
+} = require("../../Models/User/user-initial");
+const { currentTime } = require("../../Services/timestamp");
+
+const PostReview = async (req, res) => {
+  const data = req.body;
+  console.log("data", data.UID);
+  const timeStamp = currentTime();
+  //add time to post data
+  data.timestamp = timeStamp;
+  //Get results from database
+  const postResult = await QueryAddUser(data, "users");
+  return res.json(postResult);
+};
+
+// Get user details function
+const GetReviews = async (req, res) => {
+  // get from database
+  const targetUser = await QueryGetUser(req.params.id, "users");
+  console.log(targetUser);
+  if (!targetUser) {
+    return res.status(404).send({ message: "data not found" });
+  }
+  return res.json(targetUser);
+};
+
+module.exports = { PostUser, GetUser, UpdateUser };
