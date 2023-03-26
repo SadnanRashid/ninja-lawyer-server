d8879c3459abfccbf7939782859766940c299d92
SadnanRashid <sadnan.rashid.07@gmail.com>
2023-03-26 15:25:22 +0600

    update

--- Controllers/Lawyer/lawyer-search-controller.js
+++ Controllers/Lawyer/lawyer-search-controller.js
@@ -1,3 +1,4 @@
+const { query } = require("express");
 const { QuerySearchLaywer } = require("../../Models/Lawyers/lawyer-model");
 const { titleCase } = require("../../Services/capitalize");
 
@@ -11,6 +12,7 @@ const GetQueryLawyer = async (req, res) => {
   let query = {};
   // Assign key and value to query object
   query[searchKey] = searchValue;
+  //   Check if specialties
   // get from database
   const targetUser = await QuerySearchLaywer(query, "lawyers");
   console.log("targetUser");
@@ -21,23 +23,7 @@ const GetQueryLawyer = async (req, res) => {
 };
 
 // Get all lawyers details with specialties
-const GetQueryLawyerSpecialties = async (req, res) => {
-  const perams = req.query;
-  const searchKey = Object.keys(perams)[0];
-  let searchValue = Object.values(perams)[0];
-  searchValue = titleCase(searchValue);
-  console.log(searchValue);
-  let query = {};
-  // Assign key and value to query object
-  query[searchKey] = searchValue;
-  // get from database
-  const targetUser = await QuerySearchLaywer(query, "lawyers");
-  console.log("targetUser");
-  if (!targetUser) {
-    return res.status(404).send({ message: "data not found" });
-  }
-  return res.json(targetUser);
-};
+const GetQueryLawyerSpecialties = async (query) => {};
 
 module.exports = {
   GetQueryLawyer,
--- Models/Lawyers/lawyer-model.js
+++ Models/Lawyers/lawyer-model.js
@@ -15,7 +15,10 @@ const QueryUnverfiedLawyer = async (collection) => {
 // Function to call database get specific user details
 const QuerySearchLaywer = async (query, collection) => {
   try {
-    const cursor = await getCollection(collection).find(query);
+    const cursor = getCollection(collection).find(query);
+    if (cursor) {
+      console.log(cursor.toArray());
+    }
     return cursor.toArray();
   } catch (error) {
     return error;
