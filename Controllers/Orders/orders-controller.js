8dd705b8d778648b9f25e7fdc82a3ba93a35efe4
SadnanRashid <sadnan.rashid.07@gmail.com>
2023-04-18 02:57:57 +0600

    adding order apis

--- Controllers/Orders/orders-controller.js
+++ Controllers/Orders/orders-controller.js
@@ -1,10 +1,14 @@
 const { QueryPostOrder } = require("../../Models/Orders/orders-model");
+const { currentTime } = require("../../Services/timestamp");
 
 // Controller to post an order to DB
 const PostOrder = async (req, res) => {
   try {
     const lawyerID = req.params.id;
     const data = req.body;
+    data.timestamp = currentTime();
+    // Post a status with pending
+    data.status = "pending";
 
     console.log(lawyerID, data);
 
