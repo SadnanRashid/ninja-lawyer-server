f2bca93d4f543df613af9f0169cc3fa6c546f96f
SadnanRashid <sadnan.rashid.07@gmail.com>
2023-04-19 02:35:31 +0600

    adding offers api

--- Routes/orders/offer-router.js
+++ Routes/orders/offer-router.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const {
-  PostOrder,
-  GetOrders,
-} = require("../../Controllers/Orders/orders-controller");
-
-const {
   GetSpecificOffer,
   GetOffer,
   PostOffer,
 } = require("../../Controllers/Offers/offers-controller");
 
+//Offer routes
+
 // Order routes
-router.post("/add/:id", PostOrder);
-router.get("/get/:id", GetOrders);
+router.post("/add/:id", PostOffer);
+router.get("/get/specific", GetSpecificOffer);
+router.get("/get/:id", GetOffer);
 
 // return router
 module.exports = router;
