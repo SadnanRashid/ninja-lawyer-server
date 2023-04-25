86f21d281867ccb64a70ad24854f47f462a5e0c6
SadnanRashid <sadnan.rashid.07@gmail.com>
2023-04-25 22:26:15 +0600

    adding payments

---
+++ Routes/Payments/payment-router.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const {
+  PostOrder,
+  GetOrders,
+  GetUserOrders,
+  GetOrderWithID,
+  ChangeStatus,
+} = require("../../Controllers/Orders/orders-controller");
+
+// Order routes
+router.post("/add/:id", PostOrder);
+router.get("/get/:id", GetOrders);
+router.get("/get/unique/:id", GetOrderWithID);
+router.get("/user/get/:id", GetUserOrders);
+router.put("/status/change", ChangeStatus);
+
+// export router
+module.exports = router;
