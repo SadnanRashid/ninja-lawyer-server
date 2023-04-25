c21211c42b2b082b8b412e8d35daf567084a5644
SadnanRashid <sadnan.rashid.07@gmail.com>
2023-04-25 22:39:15 +0600

    adding payments

--- Models/Payments/payment-model.js
+++ Models/Payments/payment-model.js
@@ -1,2 +1,19 @@
 const { ObjectId } = require("mongodb");
 const { getCollection } = require("../database");
+
+const QueryAddPayment = async (data) => {
+  const { userID, lawyerID, amount, timestamp } = data;
+
+  if (!userID || !lawyerID || !amount || !timestamp) {
+    return { message: "Invalid request" };
+  }
+
+  const addPayment = await getCollection(collection).insertOne({
+    UserID: userID,
+    LawyerID: lawyerID,
+    amount: amount,
+    timestamp: timestamp,
+  });
+
+  return addPayment;
+};
